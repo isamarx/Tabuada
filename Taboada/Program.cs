2ddc12b77460517c7305245808ab58ba98dbf2d2
Samara Pereira <samarapds26@gmail.com>
2022-07-18 11:26:20 -0300

    Tabuada

---
+++ Taboada/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual tabuada você quer ver?");
+            int tab = Convert.ToInt16(Console.ReadLine());
+
+            {
+                for (int d = 1; d <= 10; d++)
+
+                    Console.WriteLine(tab + " x " + d + " = " + tab * d);
+
+            }
+        }
+    }
+}
